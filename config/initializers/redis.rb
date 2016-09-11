66bef6dd67815ef6b2c00306c71032e6f8e22c72
Pawan Seerwani <pawanseerwani@gmail.com>
2016-09-11 18:58:36 +0530

    redis_port

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-redis = Redis.new(:host => "pawans.housing.com", :port => "6379")
+redis = Redis.new(:host => "localhost", :port => "6379")
 $redis = Redis::Namespace.new(:new_projects, :redis => redis)
